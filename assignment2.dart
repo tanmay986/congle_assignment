b31f3e019009255e8eb83d49dab6f9e6a212ea7c
Anuj Mishra <anujfrom2002@gmail.com>
2025-04-10 13:24:53 +0530

    Both File

---
+++ assignment2.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DockScreen(),
+    );
+  }
+}
+
+class DockScreen extends StatefulWidget {
+  const DockScreen({super.key});
+
+  @override
+  State<DockScreen> createState() => _DockScreenState();
+}
+
+class _DockScreenState extends State<DockScreen> {
+  List<IconData> dockItems = [
+    Icons.person,
+    Icons.message,
+    Icons.call,
+    Icons.camera,
+    Icons.photo,
+  ];
+
+  int? draggingIndex;
+  Set<int> hoveredIndices = {};
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final dockHeight = 100.0;
+    final bottomPadding = MediaQuery.of(context).padding.bottom;
+
+    return Scaffold(
+      body: DragTarget<int>(
+        onAcceptWithDetails: (details) {
+          final globalOffset = details.offset.dy;
+          final dockTopLimit = screenHeight - dockHeight - bottomPadding;
+
+          if (globalOffset < dockTopLimit) {
+            setState(() {
+              dockItems.removeAt(details.data);
+            });
+          }
+        },
+        builder: (context, candidateData, rejectedData) {
+          return Stack(
+            children: [
+              const Center(
+                child: Text(
+                  'Tap and drag icons to rearrange or drag out to remove\nHover over icons to see effect',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: dockHeight,
+                  margin: EdgeInsets.only(
+                    bottom: bottomPadding + 16,
+                    left: 16,
+                    right: 16,
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(dockItems.length, (index) {
+                      final icon = dockItems[index];
+                      return MouseRegion(
+                        onEnter: (_) => setState(() => hoveredIndices.add(index)),
+                        onExit: (_) => setState(() => hoveredIndices.remove(index)),
+                        child: Draggable<int>(
+                          data: index,
+                          feedback: Opacity(
+                            opacity: 0.7,
+                            child: buildIcon(icon, hovered: true),
+                          ),
+                          childWhenDragging: const SizedBox(width: 56),
+                          onDragStarted: () => setState(() => draggingIndex = index),
+                          onDraggableCanceled: (_, __) => setState(() => draggingIndex = null),
+                          onDragEnd: (_) => setState(() => draggingIndex = null),
+                          child: DragTarget<int>(
+                            onWillAccept: (fromIndex) => fromIndex != index,
+                            onAccept: (fromIndex) {
+                              setState(() {
+                                final dragged = dockItems.removeAt(fromIndex);
+                                dockItems.insert(index, dragged);
+                              });
+                            },
+                            builder: (context, candidateData, rejectedData) {
+                              return buildIcon(icon, hovered: hoveredIndices.contains(index));
+                            },
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildIcon(IconData icon, {bool hovered = false}) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      margin: const EdgeInsets.symmetric(horizontal: 6),
+      width: hovered ? 56 : 48,
+      height: hovered ? 56 : 48,
+      decoration: BoxDecoration(
+        color: hovered
+            ? Colors.primaries[icon.codePoint % Colors.primaries.length].shade300
+            : Colors.primaries[icon.codePoint % Colors.primaries.length],
+        borderRadius: BorderRadius.circular(hovered ? 16 : 12),
+        boxShadow: hovered
+            ? [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 8,
+                  offset: Offset(0, 4),
+                ),
+              ]
+            : [],
+      ),
+      child: Icon(icon, color: Colors.white),
+    );
+  }
+} 
---
+++ assignment_1.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Padding(
+        padding: EdgeInsets.all(32.0),
+        child: SquareAnimation(),
+      ),
+    );
+  }
+}
+
+class SquareAnimation extends StatefulWidget {
+  const SquareAnimation({super.key});
+
+  @override
+  State<SquareAnimation> createState() => SquareAnimationState();
+}
+
+class SquareAnimationState extends State<SquareAnimation> with SingleTickerProviderStateMixin {
+  static const _squareSize = 50.0;
+
+  double xpos = 0;
+  late double leftlimit;
+  late double rightlimit;
+  bool isMoving = false;
+
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  // For hover states
+  bool isHoveringLeft = false;
+  bool isHoveringRight = false;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    animation = Tween<double>(begin: 0, end: 0).animate(controller);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final screenWidth = MediaQuery.of(context).size.width;
+    leftlimit = ((screenWidth / 2) - _squareSize / 2);
+    rightlimit = -((screenWidth / 2) - _squareSize / 2);
+  }
+
+  void moveRight() async {
+    setState(() {
+      isMoving = true;
+      animation = Tween<double>(begin: xpos, end: rightlimit)
+          .animate(CurvedAnimation(parent: controller, curve: Curves.bounceOut));
+    });
+    controller.forward(from: 0);
+    await Future.delayed(const Duration(seconds: 1));
+    setState(() {
+      xpos = rightlimit;
+      isMoving = false;
+    });
+  }
+
+  void _moveLeft() async {
+    setState(() {
+      isMoving = true;
+      animation = Tween<double>(begin: xpos, end: leftlimit)
+          .animate(CurvedAnimation(parent: controller, curve: Curves.bounceOut));
+    });
+    controller.forward(from: 0);
+    await Future.delayed(const Duration(seconds: 1));
+    setState(() {
+      xpos = leftlimit;
+      isMoving = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 200,
+          child: Center(
+            child: AnimatedBuilder(
+              animation: animation,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(animation.value, 0),
+                  child: child,
+                );
+              },
+              child: Container(
+                width: _squareSize,
+                height: _squareSize,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  border: Border.all(),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MouseRegion(
+              onEnter: (_) => setState(() => isHoveringLeft = true),
+              onExit: (_) => setState(() => isHoveringLeft = false),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: isHoveringLeft ? Colors.blueAccent : null,
+                  elevation: isHoveringLeft ? 8 : 2,
+                ),
+                onPressed: (!isMoving && xpos != rightlimit) ? moveRight : null,
+                child: const Text('Left'),
+              ),
+            ),
+            const SizedBox(width: 8),
+            MouseRegion(
+              onEnter: (_) => setState(() => isHoveringRight = true),
+              onExit: (_) => setState(() => isHoveringRight = false),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: isHoveringRight ? Colors.green : null,
+                  elevation: isHoveringRight ? 8 : 2,
+                ),
+                onPressed: (!isMoving && xpos != leftlimit) ? _moveLeft : null,
+                child: const Text('Right'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
